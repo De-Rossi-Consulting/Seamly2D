7ffc3fe73099870fd38ed71c1f40ad5a6b411294
xypp3 <32486789+xypp3@users.noreply.github.com>
2024-07-11 10:12:03 +0000

    Deploying to gh-pages from @ De-Rossi-Consulting/Seamly2D@78f973b897d0dbcd754471afba75c8b49d11c7f0 🚀

--- docs/search/classes_9.js
+++ docs/search/classes_9.js
@@ -1,9 +1,5 @@
 var searchData=
 [
-  ['layoutsettingsdialog_9725',['LayoutSettingsDialog',['../db/d32/classLayoutSettingsDialog.html',1,'']]],
-  ['linetypecombobox_9726',['LineTypeComboBox',['../d9/de2/classLineTypeComboBox.html',1,'']]],
-  ['linetypeproperty_9727',['LineTypeProperty',['../d5/daa/classVPE_1_1LineTypeProperty.html',1,'VPE']]],
-  ['lineweightcombobox_9728',['LineWeightComboBox',['../d6/d43/classLineWeightComboBox.html',1,'']]],
-  ['lineweightproperty_9729',['LineWeightProperty',['../d8/ddf/classVPE_1_1LineWeightProperty.html',1,'VPE']]],
-  ['liteparseevent_9730',['LiteParseEvent',['../d5/d08/classLiteParseEvent.html',1,'']]]
+  ['janitor_16956',['Janitor',['../d8/dc7/classJanitor.html',1,'']]],
+  ['janitormemfuncall_16957',['JanitorMemFunCall',['../d9/db9/classJanitorMemFunCall.html',1,'']]]
 ];
