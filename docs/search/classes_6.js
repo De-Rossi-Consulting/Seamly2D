0c1bf6bb107489a0fb088c0f37826c0047392222
xypp3 <xypp3@users.noreply.github.com>
2025-01-20 02:37:32 +0000

    Deploying to gh-pages from @ De-Rossi-Consulting/Seamly2D@63d2fe7485e4d6d2682eb44ef8e48e02e5698ce8 🚀

--- docs/search/classes_6.js
+++ docs/search/classes_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['graphicsviewzoom_9746',['GraphicsViewZoom',['../da/d19/classGraphicsViewZoom.html',1,'']]],
-  ['groupattributes_9747',['GroupAttributes',['../d8/d6b/structGroupAttributes.html',1,'']]],
-  ['groupswidget_9748',['GroupsWidget',['../dc/d83/classGroupsWidget.html',1,'']]],
-  ['grouptablewidgetitem_9749',['GroupTableWidgetItem',['../d9/d04/classGroupTableWidgetItem.html',1,'']]]
+  ['graphicsviewzoom_0',['GraphicsViewZoom',['../da/d19/classGraphicsViewZoom.html',1,'']]],
+  ['groupattributes_1',['GroupAttributes',['../d8/d6b/structGroupAttributes.html',1,'']]],
+  ['groupswidget_2',['GroupsWidget',['../dc/d83/classGroupsWidget.html',1,'']]],
+  ['grouptablewidgetitem_3',['GroupTableWidgetItem',['../d9/d04/classGroupTableWidgetItem.html',1,'']]]
 ];
--- docs/search/classes_b.js
+++ docs/search/classes_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['newmeasurementsdialog_9789',['NewMeasurementsDialog',['../dc/da3/classNewMeasurementsDialog.html',1,'']]],
-  ['nodeinfo_9790',['NodeInfo',['../d7/d55/structNodeInfo.html',1,'']]],
-  ['nonscalingfillpathitem_9791',['NonScalingFillPathItem',['../df/d63/classNonScalingFillPathItem.html',1,'']]],
-  ['notchdata_9792',['NotchData',['../d2/daa/structNotchData.html',1,'']]]
+  ['newmeasurementsdialog_0',['NewMeasurementsDialog',['../dc/da3/classNewMeasurementsDialog.html',1,'']]],
+  ['nodeinfo_1',['NodeInfo',['../d7/d55/structNodeInfo.html',1,'']]],
+  ['nonscalingfillpathitem_2',['NonScalingFillPathItem',['../df/d63/classNonScalingFillPathItem.html',1,'']]],
+  ['notchdata_3',['NotchData',['../d2/daa/structNotchData.html',1,'']]]
 ];
